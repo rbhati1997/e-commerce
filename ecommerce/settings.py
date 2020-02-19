c25188b9886540a593aa0f8181d643b74ab36ff4
Rohit <Rohit@gmail.com>
2020-02-19 15:31:45 +0530

    Adding celery package according to requirments.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -156,7 +156,6 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = 'product_grid'
 LOGOUT_REDIRECT_URL = 'login.html'
 
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -168,3 +167,10 @@ ACCOUNT_FORMS = {
     'signup': 'shop.forms.CustomSignupForm',
 }
 
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
